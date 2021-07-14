44c62c868746a1bacd2f3489ba7c608755eba624
Kyle Ortiz <kyleaortiz@gmail.com>
2021-07-13 19:39:54 -0500

    commit tree 70b4b015e216969dd540fece92fb70bf498c9a55

--- main.js
+++ main.js
@@ -3,6 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const emptyLikes = document.querySelectorAll(".like-glyph");
+emptyLikes.forEach((emptyLike) => {
+  emptyLike.addEventListener("click",  () => {
+    mimicServerCall()
+    .then (() => {
+      emptyLike.innerText = `${FULL_HEART}`;
+      emptyLike.classList.add("activated-heart");
+    })
+    .catch((e) => {
+      const error = document.querySelector(".hidden");
+      error.classList.remove("hidden");
+      const errorMessage = document.querySelector("#modal-message");
+      errorMessage.innerText = `${e}`;
+      setTimeout((e) => {
+        error.classList.add("hidden")
+      },3000);
+      
+    })
+  })
+})
+const fullLikes = document.querySelectorAll(".like-glyph");
+fullLikes.forEach((fullLike) => {
+  fullLike.addEventListener("click",  () => {
+    mimicServerCall()
+    .then (() => {
+      fullLike.innerText = `${EMPTY_HEART}`;
+      fullLike.classList.remove("activated-heart");
+    })
+    .catch((e) => {
+      const error = document.querySelector(".hidden");
+      error.classList.remove("hidden");
+      const errorMessage = document.querySelector("#modal-message");
+      errorMessage.innerText = `${e}`;
+      setTimeout((e) => {
+        error.classList.add("hidden")
+      },3000);
+      
+    })
+  })
+})
 
 
 
